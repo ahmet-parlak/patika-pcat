99e11976a4c37482bac77bfbe3f50ae5442503bc
ahmet-parlak <ahmet--parlak@outlook.com>
2023-09-16 00:52:30 +0300

    Create express server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`The server has started running on port ${port}..`);
+});
