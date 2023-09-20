767f9cd2ec57c48de673629d2230396c6388f8b7
ahmet-parlak <ahmet--parlak@outlook.com>
2023-09-20 17:43:09 +0300

    Integrate Mongoose, Create Photo Model
    - Integrated Mongoose into the project for database operations.
    - Designed the Photo model to store photo-related data.
    - Implemented photo creation functionality, allowing users to upload
    photos.
    - Enabled the display of photos on the main page for users to view.

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 const express = require('express');
+
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
+
 const path = require('path');
 
+const Photo = require('./models/Photo');
+
+//App
 const app = express();
+
+//DB Conntection
+mongoose
+  .connect('mongodb://localhost:27017/pcat-test-db')
+  .then(() => console.log('DB Connected'));
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', { photos });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 app.get('*', (req, res) => {
   res.render('404');
